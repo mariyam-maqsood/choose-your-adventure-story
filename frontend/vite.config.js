63249b55f397c47952015102441579ad73cd824d
mariyam-maqsood <itsmariamahmad@gmail.com>
2025-09-11 01:13:14 +0500

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig( (command,mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  console.log(env.VITE_DEBUG)
+  return {
   plugins: [react()],
   server:{
-    proxy:{
-      "/api":{
-        target: "http://localhost:8001",
-        changeOrigin: true,
-        secure: false
+    ...(env.VITE_DEBUG==="true" && {
+      proxy:{
+        "/api":{
+          target: "http://localhost:8001",
+          changeOrigin: true,
+          secure: false
+        }
       }
-    }
+    })
   }
-})
+}})
