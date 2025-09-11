24878f49454a3222dd91997809358a77c7342687
mariyam-maqsood <itsmariamahmad@gmail.com>
2025-09-11 05:10:05 +0500

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig( (command,mode) => {
     ...(env.VITE_DEBUG==="true" && {
       proxy:{
         "/api":{
-          target: "http://localhost:8001",
+          target: "http://localhost:8000",
           changeOrigin: true,
           secure: false
         }
